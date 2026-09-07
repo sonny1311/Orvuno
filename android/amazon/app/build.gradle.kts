050adcd9e2614b23315e8636c33aa927659c6dd0
sonny1311 <34922160+sonny1311@users.noreply.github.com>
2026-09-07 18:23:17 +0200

    Configure Amazon APK version 2

---
+++ android/amazon/app/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "de.nadena.orvuno"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "de.nadena.orvuno"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 2
+        versionName = "1.0.1"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            isShrinkResources = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    implementation("com.amazon.device:amazon-appstore-sdk:3.0.7")
+}
