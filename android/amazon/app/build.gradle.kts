5ab561804007e8c37e74bb74126550248783e0ef
sonny1311 <34922160+sonny1311@users.noreply.github.com>
2026-09-07 19:17:19 +0200

    Set Amazon release version code to 3

--- android/amazon/app/build.gradle.kts
+++ android/amazon/app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
         applicationId = "de.nadena.orvuno"
         minSdk = 21
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0.2"
     }
 
