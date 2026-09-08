64781f6a517f73a17d3c436e2f0fa12353d51942
sonny1311 <34922160+sonny1311@users.noreply.github.com>
2026-09-08 14:06:38 +0200

    Load robust contextual help click fix

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -3,6 +3,7 @@
 // ORVUNO
 // ============================================
 import "./core/GermanTechnicalErrorIntegration.js";
+import "./core/HelpQuestionMarkClickFixIntegration.js";
 import "./core/GlobalDarkThemeIntegration.js";
 import "./core/DarkMainNavigationIntegration.js";
 import "./core/DarkCommandCenterHeaderIntegration.js";
